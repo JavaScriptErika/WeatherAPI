6018bc102251d2c52bd69d2caabbb3e829e4ed1b
Erika Ritter <erikajritter@gmail.com>
2017-04-05 10:36:06 -0400

    Refactor object literal, cache DOM, add comments

--- js/app.js
+++ js/app.js
@@ -1,78 +1,98 @@
-//Create a Module - object literal
-var myModule = {
+(function () { //wrapped in an iife so there are no global variables to global namespace
+	var myModule = { //Object Literal Module
 
-	getPosition: function () {
-		if (!navigator.geolocation) {
-			console.log("Geolocation is not supported by your browser");
-		} else {
-			navigator.geolocation.getCurrentPosition(myModule.success, myModule.error);
-		}
-	},
+		init: function () {
+			this.cacheDOM();
+			this.getPosition();
+			this.bindEvents();
+		},
 
-	success: function (position) {
-		const lat = position.coords.latitude;
-		const lon = position.coords.longitude;
-		const urlCoordinates = lat + "," + lon;
-		myModule.geocodeRequest(urlCoordinates);
-		myModule.weatherRequest(urlCoordinates);
-	},
+		cacheDOM: function () {
+			this.$description = $("#tempDescription");
+			this.$area = $("#area");
+			this.$summary = $("#summary");
+			this.$showData = $(".info").show();
+			this.$button = $("button");
+			this.$icon = $("#icon");
+		},
 
-	error: function () {
-		$("#tempDescription").html("Unable to retrieve your location");
-	},
-
-	geocodeRequest: function (coordinates) {
-		$.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates + "&key=AIzaSyALGD0_f9SHFf-RZPqE4Ts31UJQ6C8BqE8", function (location) {
-			$("#area").html(`${location.results[0].address_components[3].long_name}, ${location.results[0].address_components[5].long_name}`);
-		});
-
-	},
-
-	weatherRequest: function (coordinates) {
-		$.get("https://api.darksky.net/forecast/a4ad1b7a7685e16ae74b44a159a83762/" + coordinates + "?exclude=minutely,hourly,daily,alerts,flags", function (data) {}).done(function (data) {
-				myModule.weatherIcon(data);
-				myModule.weatherSummary(data);
-				myModule.temperature(data);
-				$(".info").show();
-			})
-			.fail(function () {
-				$("#tempDescription").html("Sorry, please try again later")
-
-			});
-	},
-
-	weatherIcon: function (data) {
-		const iconDisplay = data.currently.icon;
-		$("#icon").attr("src", "SVG/" + iconDisplay + ".svg");
-	},
-
-	weatherSummary: function(data) {
-		$("#summary").html(data.currently.summary);
-	},
-
-	temperature: function (data) {
-		let fTemp = data.currently.apparentTemperature;
-
-		$("#tempDescription").html(`Feels like ${fTemp}&deg;F`);
-
-		myModule.convertTemp(fTemp);
-	},
-
-	convertTemp: function (fTemp) {
-		let cTemp = ((fTemp - 32) * (5 / 9));
-
-		$("button").on("click", function () {
-			$(this).text($(this).text() == 'Fahrenheit' ? 'Celcius' : 'Fahrenheit');
-			if ($(this).text() !== 'Fahrenheit') {
-				$("#tempDescription").html(`Feels like ${fTemp} &deg;F`);
+		getPosition: function () {
+			if (!navigator.geolocation) { //checks if geolocation object is not available in browser
+				this.$description.html("Geolocation is not supported by your browser");
 			} else {
-				$("#tempDescription").html(`Feels like ${cTemp.toFixed(2)} &deg;C`);
+				navigator.geolocation.getCurrentPosition(this.success.bind(this), this.error.bind(this)); //callback functions of success and error
 			}
-		});
+		},
 
-	},
+		success: function (position) { //stores latitude and longitude
+			const lat = position.coords.latitude,
+				lon = position.coords.longitude,
+				urlCoordinates = lat + "," + lon;
+			this.geocodeRequest(urlCoordinates); //google api to grab location from coordinates
+			this.weatherRequest(urlCoordinates); //dark sky api for weather conditions
+		},
 
-}
+		error: function () {
+			this.$description.html("Unable to retrieve your location");
+			this.$showData.toggle(); //if darksky api works and shows, still want to hide elements
+		},
 
+		geocodeRequest: function (coordinates) { //converts coordinates into user's city and state
+			$.get("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + coordinates + "&key=AIzaSyALGD0_f9SHFf-RZPqE4Ts31UJQ6C8BqE8", function (location) {
+				$("#area").html(`${location.results[0].address_components[3].long_name}, ${location.results[0].address_components[5].long_name}`);
+			});
 
-myModule.getPosition();
+		},
+
+		weatherRequest: function (coordinates) { //darksky api to receive weather data based on user's coordinates
+			$.get("https://api.darksky.net/forecast/a4ad1b7a7685e16ae74b44a159a83762/" + coordinates + "?exclude=minutely,hourly,daily,alerts,flags", $.proxy(function (data) {
+					//using proxy fixes context of 'this' reference
+
+					const iconDisplay = data.currently.icon;
+					const summary = data.currently.summary;
+					fTemp = data.currently.apparentTemperature; //global in the context of the object to access variable in bindEvents();
+
+					this.weatherIcon(iconDisplay);
+					this.weatherSummary(summary);
+					this.temperature(); //Will use fTemp
+					this.$showData;
+				}, this))
+				.fail(function () {
+					this.$description.html("Sorry, please try again later")
+				});
+		},
+
+		weatherIcon: function (iconDisplay) {
+			this.$icon.attr("src", "SVG/" + iconDisplay + ".svg");
+		},
+
+		weatherSummary: function (summary) {
+			this.$summary.html(summary);
+		},
+
+		temperature: function () { //uses fTemp from goval declaration(in context of object)
+			this.$description.html(`Feels like ${fTemp}&deg;F`);
+		},
+
+		bindEvents: function () {
+			this.$button.on("click", this.convertTemp.bind(this));
+		},
+
+		//Receives fTemp from global declaration (in context of object)
+		convertTemp: function () {
+			let cTemp = ((fTemp - 32) * (5 / 9));
+			//ternary operator to change the text of button when clicked
+			this.$button.text(this.$button.text() == 'Fahrenheit' ? 'Celcius' : 'Fahrenheit');
+			//When button does not say Fahrenheit (since it's already converted to that), show temp in Fahrenheit from temperature func, else show Celsius
+			if (this.$button.text() !== 'Fahrenheit') {
+				this.temperature(fTemp);
+			} else {
+				this.$description.html(`Feels like ${cTemp.toFixed(2)} &deg;C`);
+			}
+		},
+
+	}
+
+	myModule.init();
+
+})();
